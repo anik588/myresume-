fb558793badb4f85894939fc7f73daf7608889b5
anik588 <sajjad.anik24@gmail.com>
2024-07-13 23:21:07 +0600

    added my name,photo and all html pages with statics

--- myresume/myresume/settings.py
+++ myresume/myresume/settings.py
@@ -118,13 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS =[
-    os.path.join(BASE_DIR , 'static')
+    os.path.join(BASE_DIR, 'static/assets/')
 ]
-STATIC_ROOT =  os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
-MEDIA_ROOT  = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles/media')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
